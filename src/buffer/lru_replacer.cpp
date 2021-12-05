f7d0410108ec9fe471e301dc5b325ea276f16609
Miller Liang <841985944@qq.com>
2021-12-05 15:37:48 +0800

    LRU Replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -12,13 +12,45 @@
 
 #include "buffer/lru_replacer.h"
 
+
+
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+struct ListNode {
+    frame_id_t frame_id{-1};
+    struct ListNode *prev{nullptr};
+    struct ListNode *next{nullptr};
+    explicit ListNode(frame_id_t frame_id) : frame_id(frame_id){}
+};
 
-LRUReplacer::~LRUReplacer() = default;
+struct ListNode *headNode;
+struct ListNode *tailNode;
+int capacity;
+int size;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+LRUReplacer::LRUReplacer(size_t num_pages) {
+    headNode = new ListNode(0);
+    tailNode = new ListNode(0);
+    headNode->next = tailNode;
+    tailNode->prev = headNode;
+
+    capacity = num_pages;
+    size = 0;
+}
+
+LRUReplacer::~LRUReplacer() {
+    ListNode *next;
+    ListNode *current = headNode;
+    for(int i = 0; i < size + 2; i++) {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+};
+
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+    lru_latch
+}
 
 void LRUReplacer::Pin(frame_id_t frame_id) {}
 
